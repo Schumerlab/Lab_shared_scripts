db21985a76d88181e7e364c56fb8fa2ea5622dcf
Gabriel Preising <gabep@DN0a25a13b.SUNet>
2023-11-08 12:58:17 -0800

    fixed typo in --help

--- mpileup_out_to_plink_format.py
+++ mpileup_out_to_plink_format.py
@@ -4,7 +4,7 @@ import pandas as pd
 parser = argparse.ArgumentParser(description="This script will convert the output of samtools mpileup (variant calls for GWAS) to a format that is suitable for bcftools to generate pseudohaploid calls. The input file should contain the following columns:\nColumn 1 = name of the chromosome/scaffold in your genome assembly of choice\nColumn 2 = pos\nColumn 3 = ref allele,alt allele\nSee https://openwetware.org/wiki/Schumer_lab:_Commonly_used_workflows#Case/Control_GWAS_from_low_coverage_data for more information.",
 	formatter_class = argparse.ArgumentDefaultsHelpFormatter)
 
-parser.add_argument("-i", "--infile", help="infile - raw pileup variant calls", required=True)
+parser.add_argument("-i", "--infile", help="infile - raw mpileup variant calls", required=True)
 parser.add_argument("-o", "--outfile", help="outfile - reformatted mpileup variant calls suitable for psuedohaploid analyses ", required=True)
 
 args = parser.parse_args()
